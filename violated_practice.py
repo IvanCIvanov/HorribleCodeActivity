3223b163dccb2efd9605131aa5b6b387256b1395
Aidan Oback <aidanoback@gmail.com>
2025-06-24 17:43:21 -0400

    docs:  moved program comment from code to README

--- violated_practice.py
+++ violated_practice.py
@@ -1,19 +1,5 @@
 # Calculator Project - Violated Practice Version
 # Group Members - Carter Meadows, Aidan Oback
-
-# Practices Violated: Documentation, KISS, DRY, and Single Responsibility
-
-# KISS is violated in the calculator functions by not keeping the code simple and
-# repeating lines that could be condensed.
-#
-# Single Responsibility is violated by the functions each doing multiple tasks,
-# such as taking inputs, type casting the inputs, and then performing the calculation.
-#
-# Documentation is violated by the lack of comments and unclear names of each function.
-#
-# DRY (Don't Repeat Yourself) is being violated here by grabbing user input multiple times,
-# repeating the prompt over and over.
-
 def function1():
     a = input("Please enter a number: ")
     b = input("Please enter a number: ")
